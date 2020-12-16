196c7224c0345f8d95f313430eab068cc92d666a
JT5519 <joel.sk10@gmail.com>
2020-12-17 01:30:35 +0530

    added solar system

---
+++ Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+//this script is only added to the camera, that is waiting for a target
+public class LookAtTarget : MonoBehaviour {
+
+	static public GameObject target; // the target that the camera should look at
+
+	void Start () {
+		if (target == null) 
+		{
+			target = this.gameObject;
+			Debug.Log ("LookAtTarget target not specified. Defaulting to parent GameObject");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target)
+			transform.LookAt(target.transform);
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView-(Input.GetAxis("Mouse ScrollWheel")*10), target.transform.localScale.x, 1000);
+	}
+}
---
+++ Assets/Scripts/MainMenuControls.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainMenuControls : MonoBehaviour
+{
+    public void onPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void onQuit()
+    {
+        Application.Quit();
+    }
+    public void onBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+      
+}
